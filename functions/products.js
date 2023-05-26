562db1c2dd3a4c3471b9d53b06ee213db44720bd
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-05-26 09:15:24 +0300

    first commit

---
+++ functions/products.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const Airtable = require("airtable-node");
+
+const airtable = new Airtable({ apikey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE)
+  .table(process.env.AIRTABLE_TABLE);
+
+exports.handler = async (event, context, cb) => {
+  try {
+    const response = await airtable.list({ maxRecords: 200 });
+    console.log("########");
+    console.log(response);
+    console.log("########");
+    return {
+      statusCode: 200,
+      body: "products route",
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: "There was an error",
+    };
+  }
+};
